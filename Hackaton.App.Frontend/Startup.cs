70dab47a0dacdff12173434353b3130758f8d2f0
DiegoRCasallas <master.drcd@hotmail.com>
2021-11-28 18:25:03 -0500

    reparacion 1.1.1

--- Hackaton.App.Frontend/Startup.cs
+++ Hackaton.App.Frontend/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Hackaton.App.Persistencia.AppRepositorios;
 using Microsoft.Extensions.Hosting;
 
 namespace Hackaton.App.Frontend
@@ -24,6 +25,8 @@ namespace Hackaton.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<RepositoriouGerencias, RepositoriouGerencias>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
